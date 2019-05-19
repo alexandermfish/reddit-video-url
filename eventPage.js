d94abdec2dd18ba823ad38729c5fe57416d4ddbb
ALEXFISH\Alex <alexandermfish@gmail.com>
2019-05-20 02:26:33 +1000

    work on functions

--- eventPage.js
+++ eventPage.js
@@ -1,13 +1,16 @@
 
+
 var showForPages = ["*://www.reddit.com/r/*/comments/*"];
-var jsonPage = "https://www.google.com";
+
+var vidURL="www.blackle.com"
+var jsonPage = "https://www.google.com"
 
 
-chrome.tabs.create({url: jsonPage}, callback);
 
 function callback(data){
   console.log(data);
 }
+
 
 var contextMenuItem ={
   "id": "getVid",
@@ -16,3 +19,27 @@ var contextMenuItem ={
 };
 
 chrome.contextMenus.create(contextMenuItem);
+chrome.contextMenus.onClicked.addListener(function(clickData){
+  if(clickData.menuItemId == "getVid"){
+    chrome.tabs.getSelected(null,function(tab) {
+      var tablink = tab.url;
+      jsonPage = tablink + ".json"
+      requestVid()
+      chrome.tabs.create({url: vidURL}, callback);
+    });
+  }
+})
+
+
+
+function requestVid() {
+  fetch(jsonPage)
+  .then(response => response.json())
+  .then(data => {
+    var redditJSON = JSON.parse();
+    vidURL = redditJSON.fallback_url;
+    console.log(data) // Prints result from `response.json()` in getRequest
+  })
+  .catch(error => console.error(error))
+  
+}
