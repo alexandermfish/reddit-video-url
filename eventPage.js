ed59d5af673c83faf6d3264ec781e468a14a1990
ALEXFISH\Alex <alexandermfish@gmail.com>
2019-05-20 00:06:35 +1000

    added some placeholder code to the javascript

--- eventPage.js
+++ eventPage.js
@@ -1,9 +1,17 @@
 
 var showForPages = ["*://www.reddit.com/r/*/comments/*"];
+var jsonPage = "https://www.google.com";
+
+
+chrome.tabs.create({url: jsonPage}, callback);
+
+function callback(data){
+  console.log(data);
+}
 
 var contextMenuItem ={
   "id": "getVid",
-  "title": "Get Reddit Vid",
+  "title": "Video Url for reddit",
   "documentUrlPatterns":showForPages
 };
 
